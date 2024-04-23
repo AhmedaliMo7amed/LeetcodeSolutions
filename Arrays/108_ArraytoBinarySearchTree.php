899b20e1bf4d6fd338f081bbdcdc6139082a0ec3
CODE <99538735+AhmedaliMo7amed@users.noreply.github.com>
2024-04-23 02:15:57 +0200

    Create 108_ArraytoBinarySearchTree.php

---
+++ Arrays/108_ArraytoBinarySearchTree.php
@@ -0,0 +1,42 @@
+<?php 
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return TreeNode
+     */
+    function sortedArrayToBST(array $nums) {
+        // Check array is empty or not
+        if (empty($nums))
+            return null;
+        
+        // get count of elements in $nums array 
+        $countOfElements = count($nums);
+
+        // get center of the array
+        $middleIndex = floor($countOfElements / 2);
+        // create the root node of the tree
+        $rootNode = new TreeNode($nums[$middleIndex]);
+
+        // rec repeat to create left side tree
+        $rootNode->left = $this->sortedArrayToBST(array_slice($nums, 0, $middleIndex));
+        // rec repeat to create right side tree
+        $rootNode->right = $this->sortedArrayToBST(array_slice($nums, $middleIndex + 1));
+
+        return $rootNode;
+    }
+}
+?>
