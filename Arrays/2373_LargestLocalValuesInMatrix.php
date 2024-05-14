f423184073fb1539f62f9d75bfe4601722720897
CODE <99538735+AhmedaliMo7amed@users.noreply.github.com>
2024-05-15 00:40:39 +0300

    Update 2373_LargestLocalValuesInMatrix.php

--- Arrays/2373_LargestLocalValuesInMatrix.php
+++ Arrays/2373_LargestLocalValuesInMatrix.php
@@ -12,12 +12,23 @@ class Solution {
 
         for ($row = 1; $row < $n - 1; $row++) {
             for ($col = 1; $col < $n - 1; $col++) {
+
+
+                // before 
+                // $submatrix = [
+                //     $grid[$row - 1][$col - 1], $grid[$row - 1][$col], $grid[$row - 1][$col + 1],
+                //     $grid[$row][$col - 1],     $grid[$row][$col],     $grid[$row][$col + 1],
+                //     $grid[$row + 1][$col - 1], $grid[$row + 1][$col], $grid[$row + 1][$col + 1]
+                // ];
+
+                // after
                 $submatrix = [];
                 for ($subRow = -1; $subRow <= 1; $subRow++) {
                     for ($subCol = -1; $subCol <= 1; $subCol++) {
                         $submatrix[] = $grid[$row + $subRow][$col + $subCol];
                     }
                 }
+                
                 $maxLocal[$row - 1][$col - 1] = max($submatrix);
             }
         }
